b5664bed1efe1d6f1bb321687b84ce0bba1342aa
“Nahuel <nnahuell.gb@hotmail.com>
2023-08-05 00:28:43 -0300

    feat:carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import { Nav } from './components/Nav';
 import { Card } from './components/Card';
-import { Carousel } from './components/Carousel';
+import { Carousel2 } from './components/Carousel';
 function App() {
 
 
@@ -11,7 +11,7 @@ function App() {
     <>
       <Nav/>
       <Card/>
-      <Carousel/>
+      <Carousel2/>
     </>
   )
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
-export const Carousel = () => {
+export const Carousel2 = () => {
+  const cityGroup = [
+    {
+      index: "1",
+      name: "trelew",
+      urls: ["/trelew1.jpg", "/trelew2.jpg"],
+    },
+    {
+      index: "2",
+      name: "trelew",
+      urls: ["/trelew3.jpg", "/trelew4.jpg"],
+    },
+    {
+      index: "3",
+      name: "trelew",
+      urls: ["/trelew5.jpg", "/trelew6.jpg"],
+    },
+    // ...
+  ];
+
   return (
- 
-    
-    <div></div>
-  )
-}
+    <div className="flex">
+      <Carousel
+        showArrows={true}
+        showThumbs={false}
+        autoPlay={true}
+        interval={2000}
+        infiniteLoop
+        width={"50%"}
+      >
+        {cityGroup.map((city, index) => (
+          <div className="grid grid-cols-2 gap-4" key={index}>
+            <img
+              src={city.urls[0]}
+              alt={city.name}
+              className="w-full h-60 rounded"
+            />
+            <img
+              src={city.urls[1]}
+              alt={city.name}
+              className="w-full h-60 rounded"
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
