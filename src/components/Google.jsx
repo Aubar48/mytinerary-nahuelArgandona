594c13d60087b08a947778ec955a2eba16b8e1d0
“Nahuel <nnahuell.gb@hotmail.com>
2023-09-12 16:24:04 -0300

    fixed all

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -1,18 +1,20 @@
 import { useRef, useEffect } from "react";
 import apiUrl from "../apiUrl";
 import axios from "axios";
-export default function Google() {
+
+export default function () {
   let googleButton = useRef();
   const handleCredentialResponse = async (response) => {
     console.log(response);
     const data = { token_google: response.credential };
-    const userResponse = await axios.post(apiUrl + "/auth/google", data);
+    const userResponse = await axios.post(apiUrl + "auth/google", data);
     console.log(userResponse);
   };
   useEffect(() => {
     window.onload = function () {
+      /* global google */
       window.google.accounts.id.initialize({
-        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        client_id: import.meta.env.VITE_GOOGLE_ID,
         callback: handleCredentialResponse,
       });
       window.google.accounts.id.renderButton(googleButton.current, {
