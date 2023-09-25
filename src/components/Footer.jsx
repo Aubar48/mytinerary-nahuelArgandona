a50b7eab6444ef74b7165ac06db084a968d7986f
“Nahuel <nnahuell.gb@hotmail.com>
2023-09-25 16:38:18 -0300

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,13 +38,13 @@ export const Footer = () => {
 
         <nav className="flex md:space-x-4 text-base mt-4 md:mt-0 max-sm:gap-2 max-sm:py-2 max-xl:mr-10 xl:mr-36 font-serif">
           <NavLink
-            className="focus:bg-sky-700 hover:bg-secondary rounded-xl py-2 px-4 bg-white hover:text-yellow-300 border-solid items-center "
+            className="focus:bg-secondary hover:bg-secondary rounded-xl py-2 px-4 bg-white hover:text-yellow-300 border-solid items-center "
             to={"/"}
           >
             Home
           </NavLink>
           <NavLink
-            className="focus:bg-sky-700 hover:bg-secondary rounded-xl py-2 px-4 bg-white hover:text-yellow-300 items-center "
+            className="focus:bg-secondary hover:bg-secondary rounded-xl py-2 px-4 bg-white hover:text-yellow-300 items-center "
             to={"/cities"}
           >
             Cities
