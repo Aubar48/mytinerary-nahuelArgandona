cf86453a89fa07723eda1a41bfac18b4e1f3459c
“Nahuel <nnahuell.gb@hotmail.com>
2023-09-25 16:26:39 -0300

    fix

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -14,7 +14,7 @@ export default function Label({ options }) {
               <Anchor
                 key={each.to}
                 to={each.to}
-                className="h-[50px] text-[20px] hover:text-yellow-300 focus:bg-sky-700 px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 "
+                className="h-[50px] text-[20px] hover:text-yellow-300 focus:bg-secondary px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 "
               >
                 {each.title}
               </Anchor>
@@ -22,7 +22,7 @@ export default function Label({ options }) {
               <Anchor
                 key={each.to}
                 to={each.to}
-                className="h-[50px] text-[20px] hover:text-yellow-300 focus:bg-sky-700 px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 font-serif"
+                className="h-[50px] text-[20px] hover:text-yellow-300 focus:bg-secondary px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 font-serif"
               >
                 {each.title}
               </Anchor>
@@ -31,7 +31,7 @@ export default function Label({ options }) {
               <span
                 key={each.title}
                 onClick={each.onClick}
-                className="h-[50px] text-[20px] hover:text-yellow-300  focus:bg-sky-700  px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 cursor-pointer font-serif"
+                className="h-[50px] text-[20px] hover:text-yellow-300  focus:bg-secondary px-2 bg-white hover:bg-secondary rounded-xl w-[100px] flex justify-center items-center mx-1 cursor-pointer font-serif"
               >
                 {each.title}
               </span>
